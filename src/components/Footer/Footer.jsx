f2f78c7e5546f1208324319f86372414edb916f9
Katherine Vilinski-Mazur <Katherine.Vilinski-Mazur@skoltech.ru>
2023-06-05 14:39:52 +0300

    +footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
 
 function Footer(props) {
-    return (
-        <footer class="footer">
-        </footer>
-    );
+  return (
+    <footer className="bg-light text-center">
+      <div className="container p-4 pb-0">
+        <section className="">
+          <form action="">
+            <div className="row d-flex justify-content-center">
+              <div className="col-auto">
+                <p className="pt-2">
+                  <strong>Sign up for our newsletter</strong>
+                </p>
+              </div>
+
+              <div className="col-md-5 col-12"></div>
+
+              <div className="col-auto">
+                <button type="submit" className="btn btn-primary mb-4">
+                  Subscribe
+                </button>
+              </div>
+            </div>
+          </form>
+        </section>
+      </div>
+
+      <div
+        className="text-center p-3"
+        style="background-color: rgba(0, 0, 0, 0.2);"
+      >
+        © 2023 Copyright:
+        <a className="text-dark" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
